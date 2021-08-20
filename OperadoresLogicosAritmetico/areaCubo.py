be234c59943826527af725b68727d2371babdec2
Renata Pedroso <renatanicollyy1@gmail.com>
2021-08-20 11:51:29 -0300

    first commit

---
+++ OperadoresLogicosAritmetico/areaCubo.py
@@ -0,0 +1,5 @@
+# Calcular a área de um cubo
+lado = input("Digite o lado da face do cubo: ")
+Af = lado ** 2
+area_total = 6 * Af
+print(area_total)
---
+++ OperadoresLogicosAritmetico/input.py
@@ -0,0 +1,8 @@
+# Faça a leitura pelo teclado de três variáveis (a, b e c) e mostre o resultado para: a + b - c + (a * c) / a ** 2
+
+a = int(input('a: '))
+b = int(input('b: '))
+c = int(input('c: '))
+
+resultado = a + b - c + (a * c) / a ** 2
+print(resultado)
